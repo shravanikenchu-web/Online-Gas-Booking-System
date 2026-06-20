7c6ecede27ed48290f0b059c9c40b18aa1491a60
shravanikenchu-web <shravanikenchu@gmail.com>
2026-06-20 15:59:44 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,31 +1,26 @@
 import sqlite3
 
 conn = sqlite3.connect('gas_booking.db')
+
 cursor = conn.cursor()
 
-# USERS TABLE
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    email TEXT UNIQUE,
+    email TEXT,
     password TEXT
 )
 ''')
-
-# BOOKINGS TABLE (FIXED - now linked to user)
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS bookings(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
     cylinder_type TEXT,
     amount TEXT,
-    status TEXT,
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    status TEXT
 )
 ''')
-
 conn.commit()
 conn.close()
 
-print("Database Created Successfully with Relations")
+print("Database Created Successfully")
