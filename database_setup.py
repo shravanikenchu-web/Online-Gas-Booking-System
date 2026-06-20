fbf432a2c1ff164231faa9ec2eddfc25568233eb
shravanikenchu-web <shravanikenchu@gmail.com>
2026-06-20 16:56:59 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,26 +1,33 @@
 import sqlite3
 
+# Create Database
 conn = sqlite3.connect('gas_booking.db')
-
 cursor = conn.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users(
+# Create Users Table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    email TEXT,
-    password TEXT
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
 )
-''')
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS bookings(
+""")
+
+# Create Bookings Table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS bookings (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    cylinder_type TEXT,
-    amount TEXT,
-    status TEXT
+    cylinder_type TEXT NOT NULL,
+    amount TEXT NOT NULL,
+    status TEXT NOT NULL
 )
-''')
+""")
+
+# Save Changes
 conn.commit()
+
+# Close Connection
 conn.close()
 
 print("Database Created Successfully")
